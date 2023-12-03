5e1cb9e2a27d8356497c4f4fd354027352b7aff2
whoiswhore <sultanfiki403@gmail.com>
2023-12-04 03:44:04 +0700

    add
    add

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,12 +19,36 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/:date', (req, res) => {
+  const { date } = req.params;
 
+  let inputDate;
+  console.log(inputDate);
+  if (!isNaN(date)) {
+    // If it's a number, assume it's a timestamp
+    inputDate = new Date(parseInt(date));
+  } else {
+    // Otherwise, assume it's a date string
+    inputDate = new Date(date);
+  }
+
+  if (isNaN(inputDate.getTime())) {
+    // Invalid date
+    return res.json({ error: 'Invalid date' });
+  }
+
+  const output = {
+    unix: inputDate.getTime(),
+    utc: inputDate.toUTCString(),
+  };
+
+  res.json(output);
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
